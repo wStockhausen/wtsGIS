a68887e3418b74bd8bb294ac1f27b0d75765cfb4
wStockhausen <william.stockhausen@noaa.gov>
2018-10-21 17:15:30 -0700

    Changes to incorporate shift to simple features (sf).

--- R/tmap.CreateBaseMap.R
+++ R/tmap.CreateBaseMap.R
@@ -4,20 +4,24 @@
 #' @description This function creates a basemap layer for maps based on the tmap package.
 #'
 #' @details The basemap contains a land layer (polygons) and a bathymetry layer (lines).
-#' Uses \code{wtsUtilities::tmap.CreateLayerFromShapefile}.
+#' Uses \code{wtsUtilities::tmap.CreateLayerFromShapefile}. If boundingbox is NULL, then the
+#' bounding box for the land layer is used as the bounding box for the basemap.
 #'
 #' @param layer.land - a tmap layer representing land
 #' @param layer.bathym - a tmap layer representing bathymetry
 #' @param gisDir - path to top level folder for shapefiles
 #' @param shapeFile.land - land shapefile (if layer.land is not provided)
 #' @param shapeFile.bathymetry - bathymetry shapefile (if layer.bathym is not provided)
-#' @param strCRS - string representation of final CRS (default = WGS84) used for ALL shapefiles
+#' @param strCRS.orig - string representation of original CRS (default = WGS84) used for ALL shapefiles
+#' @param strCRS.finl- string representation of final CRS (default = WGS84) used for the basemap
 #' @param boundingbox - a tmap-style bounding box
 #' @param colors.bathym - color for the bathymetry
 #' @param points.size - size of points, in map units
 #' @param verbose - flag to print debugging info
 #'
 #' @return - basemap layer based on the tmap package
+#'
+#' @import magrittr
 #'
 #' @export
 #'
@@ -26,7 +30,8 @@ tmap.CreateBaseMap<-function( layer.land=NULL,
                               gisDir=NULL,
                               shapeFile.land      =system.file("extdata/Shapefiles/Land/Alaska.shp",package="wtsGIS"),
                               shapeFile.bathymetry=system.file("extdata/Shapefiles/Bathymetry/ShelfBathymetry.shp",package="wtsGIS"),
-                              strCRS=tmaptools::get_proj4("longlat",output="character"),
+                              strCRS.orig=tmaptools::get_proj4("longlat",output="character"),
+                              strCRS.finl=tmaptools::get_proj4("longlat",output="character"),
                               boundingbox=list(bottomleft=list(lon=-179,lat=54),
                                                topright  =list(lon=-157,lat=62.5)),
                               colors.bathym="darkblue",
@@ -40,7 +45,9 @@ tmap.CreateBaseMap<-function( layer.land=NULL,
       f<-shapeFile.land;
       if (!is.null(gisDir)) f<-file.path(gisDir,shapeFile.land);
       if (verbose) cat("reading land shapefile '",f,"'\n",sep="")
-      land<-tmap.CreateLayerFromShapefile(f,strCRS=strCRS);
+      land<-tmap.CreateLayerFromShapefile(f,strCRS=strCRS.orig);
+      if (!is.null(strCRS.finl)&&(strCRS.finl!=strCRS.orig))
+        land <- land %>% tmaptools::set_projection(projection=strCRS.finl);
     }
   }
 
@@ -50,22 +57,27 @@ tmap.CreateBaseMap<-function( layer.land=NULL,
       f<-shapeFile.bathymetry;
       if (!is.null(gisDir)) f<-file.path(gisDir,shapeFile.bathymetry);
       if (verbose) cat("reading bathymetry shapefile '",f,"'\n",sep="")
-      bathym<-tmap.CreateLayerFromShapefile(f,strCRS=strCRS);
+      bathym<-tmap.CreateLayerFromShapefile(f,strCRS=strCRS.orig);
+      if (!is.null(strCRS.finl)&&(strCRS.finl!=strCRS.orig))
+        bathym <- bathym %>% tmaptools::set_projection(projection=strCRS.finl);
     }
   }
 
 
   #define bounding box for map extent
   bbext<-tmaptools::bb(land);#just to get a bounding box
-  bbext['xmin']<-boundingbox$bottomleft$lon;
-  bbext['ymin']<-boundingbox$bottomleft$lat;
-  bbext['xmax']<-boundingbox$topright$lon;
-  bbext['ymax']<-boundingbox$topright$lat;
+  if (!is.null(boundingbox)){
+    bbext['xmin']<-boundingbox$bottomleft$lon;
+    bbext['ymin']<-boundingbox$bottomleft$lat;
+    bbext['xmax']<-boundingbox$topright$lon;
+    bbext['ymax']<-boundingbox$topright$lat;
+  }
 
   #basemap using CRS from strCRS
-  basemap<-tmap::tm_shape(land,bbox=bbext,is.master=TRUE)+tmap::tm_fill();
+  basemap<-tmap::tm_shape(land,bbox=bbext,is.master=TRUE,projection=strCRS.finl)+
+             tmap::tm_fill();
   if (!is.null(bathym))
-      basemap <- basemap + tmap::tm_shape(bathym)+tmap::tm_lines(col=colors.bathym);
+      basemap <- basemap + tmap::tm_shape(bathym) + tmap::tm_lines(col=colors.bathym);
 
   return(basemap);
 }
--- R/tmap.MergeDataframeWithLayer.R
+++ R/tmap.MergeDataframeWithLayer.R
@@ -6,13 +6,13 @@
 #' @details
 #'
 #' @param dfr - dataframe to merge
-#' @param geoms - tmap geometry layer to merge with
+#' @param geoms - geometries to merge with
 #' @param dataID - name of id column in dataframe to join on
 #' @param geomsID - name of id column in geometry layer to join on
 #' @param allData - flag to merge all rows from dataframe
 #' @param duplicateGeoms - allow geoms to be duplicated (i.e., where multiple rows in dataframe maatch a single geom)
 #'
-#' @return - merged tmap layer
+#' @return - merged spatial dataframe (based on sf or sp classes, depending on class of geoms)
 #'
 #' @export
 #'
@@ -23,21 +23,25 @@ tmap.MergeDataframeWithLayer<-function(dfr,
                                        allData=FALSE,
                                        duplicateGeoms=TRUE){
   #join annual dfr to geoms by matching ID values
-  dfr.geoms<-sp::merge(geoms,dfr,
-                      by.x=dataID,
-                      by.y=geomsID,
-                      all.x=allData,
-                      duplicateGeoms=duplicateGeoms);
-  if ("polygons" %in% slotNames(geoms)){
-    #assign unique FIDs to polygons (merge above does not assign unique IDs to duplicate geoms)
-    dfr.geoms<-sp::spChFIDs(dfr.geoms,
-                            as.character(1:length(dfr.geoms@polygons)));
+  if (inherits(geoms,c("sf","sfc","sfg"))){
+    # str<-paste("geoms are simple features of class",class(geoms),"\n");
+    # str<-c(str,
+    #        "wtsGIS::tmap.MergeDataframeWithLayer() not yet implemented for simple features (sf) geometries.\n");
+    # stop(str);
+    dr.geoms<-sf::left_join(dfr,geoms,
+                            by=c(dataID=geomsID));
+  } else {
+    #use sp functions
+    dfr.geoms<-sp::merge(geoms,dfr,
+                        by.x=dataID,
+                        by.y=geomsID,
+                        all.x=allData,
+                        duplicateGeoms=duplicateGeoms);
+    if ("polygons" %in% slotNames(geoms)){
+      #assign unique FIDs to polygons (merge above does not assign unique IDs to duplicate geoms)
+      dfr.geoms<-sp::spChFIDs(dfr.geoms,
+                              as.character(1:length(dfr.geoms@polygons)));
+    }
   }
-#  #join annual evs to stns.pnts by station
-#  evs.pnts<-sp::merge(stns.pnts,evs.csv,
-#                    by.x="ID",by.y="GIS_STATION",
-#                    all.x=FALSE,duplicateGeoms=TRUE);
-#  #assign unique FIDs to polygons (merge above does not assign unique IDs to duplicate geoms)
-#  #evs.pnts<-sp::spChFIDs(evs.pnts,as.character(1:length(evs.pnts@coords)));
   return(dfr.geoms);
 }
