6355f4033357f36244d70cc4b2b571fe24894769
wStockhausen <william.stockhausen@noaa.gov>
2020-01-24 13:28:43 -0800

    Corrected error in writeToShapefile and added error message when column names exceed 10 characters.

--- R/writeToShapefile.R
+++ R/writeToShapefile.R
@@ -9,15 +9,22 @@
 #' @return none
 #'
 #' @details Wrapper for \code{sf::st_write(...)} or \code{rgdal::writeOGR(...)}.
+#' Note that column names are limited to 10 characters max.
 #'
 #' @export
 #'
 writeToShapefile<-function(obj,
                            file="shapefile"){
+  if (inherits(obj,"data.frame")){
+    if (any(nchar(names(obj))>10)) {
+      msg<-"wtsGIS::writeToShapefile: cannot have column names longer than 10 characters.";
+      stop(msg);
+    }
+  }
   folder<-dirname(file);
   layer<-basename(file);
   if (stringi::stri_sub(layer,from=-4,to=-1)==".shp") layer<-stringi::stri_sub(layer,from=1,to=-5);
   if (!dir.exists(folder)) dir.create(folder,recursive=TRUE);
-  writeToGISDataset(obj,dsn=dirname,layer=layer,driver="ESRI Shapefile",delete_layer=TRUE);
+  writeToGISDataset(obj,dsn=folder,layer=layer,driver="ESRI Shapefile",delete_layer=TRUE);
   return(invisible(NULL));
 }
